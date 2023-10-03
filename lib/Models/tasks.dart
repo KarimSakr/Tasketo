9486654c5572be195477164a2ab1b3223930ec39
Karim Sakr <karimsakr000@gmail.com>
2023-10-04 00:25:31 +0300

    Added task models and tag enum

---
+++ lib/Models/tasks.dart
@@ -0,0 +1,22 @@
+import 'package:tasketo/Models/user.dart';
+import 'package:tasketo/Utils/Enums/tag.dart';
+
+class Tasks {
+    Tasks({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.assignedUser,
+    required this.assignedDate,
+    required this.dueDate,
+    required this.tag,
+  });
+  
+  final String id;
+  final String title;
+  final String description;
+  final TasketoUser assignedUser;
+  final DateTime assignedDate;
+  final DateTime dueDate;
+  final Tag tag;
+}
--- lib/Views/Task/task_view.dart
+++ lib/Views/Task/task_view.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:tasketo/Models/tasks.dart';
 
-class TaskView extends StatelessWidget {
+class TaskView extends StatefulWidget {
   const TaskView({super.key});
 
   @override
+  State<TaskView> createState() => _TaskViewState();
+}
+
+class _TaskViewState extends State<TaskView> {
+  List<Tasks> tasks = [];
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Task View'),
-    );
+    return ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+        });
   }
 }
